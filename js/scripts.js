bb110279843ee7e4e3489cba04883b1ee9f99900
Harshial <hjs78@drexel.edu>
2023-12-15 23:27:18 -0500

    week8

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //alert("Hello Class!");
 
 var mainNav = document.querySelector("#mainNav");
-
+var signInUpBtn = document.querySelector("#sign-in-up-btn")
 var burgerBtn = document.querySelector("#burger-container button");
 console.log(burgerBtn);
 
@@ -12,9 +12,15 @@ function showNav(){
    console.log("button clicked");
     if(canYouSeeMainNav === false){
         mainNav.classList.add("showMainNav");
+
+        signInUpBtn.classList.add("showSignIn");
         canYouSeeMainNav = true;
     }else{
         mainNav.classList.remove("showMainNav");
+
+        signInUpBtn.classList.remove("showSignIn");
+
+
         canYouSeeMainNav = false;
     }
    
@@ -24,3 +30,15 @@ function showNav(){
 burgerBtn.addEventListener("click", showNav);
 //showNav();
 
+var signInBtn = document.querySelector("#sign-btn");
+
+var signInContainer = document.querySelector("#sign-in-container");
+
+
+function showSignInModal(){
+//console.log("show modal now");
+
+signInContainer.classList.add("#Show-sign-in-container");
+}
+
+SignInBtn.addEventListener("click", showSignInModal);
